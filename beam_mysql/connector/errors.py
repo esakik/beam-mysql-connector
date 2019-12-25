b9478a196e1f92dc5ba05656f0612dbe3c4a2b5f
esaki01 <social0430@gmail.com>
2019-12-26 04:50:39 +0900

    Add beam mysql errors

---
+++ beam_mysql/connector/errors.py
@@ -0,0 +1,9 @@
+"""Beam mysql error classes."""
+
+
+class BeamMySQLError(Exception):
+    """Base class for all Beam MySQL errors."""
+
+
+class MySQLClientError(BeamMySQLError):
+    """An error in the mysql client object (e.g. failed to execute query)."""
