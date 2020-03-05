f39ae89ef8d42c53b5c1c59e2f7d8d11cd31c9c1
esaki01 <social0430@gmail.com>
2020-03-05 12:41:26 +0900

    [Bugfix] Support static value provider

--- beam_mysql/connector/utils.py
+++ beam_mysql/connector/utils.py
@@ -2,11 +2,11 @@
 
 from typing import Any
 
-from apache_beam.options.value_provider import RuntimeValueProvider
+from apache_beam.options.value_provider import ValueProvider
 
 
 def get_runtime_value(value: Any) -> Any:
-    if isinstance(value, RuntimeValueProvider):
+    if isinstance(value, ValueProvider):
         return value.get()
     else:
         return value
