f45388e20855e61593613b327bd38eaacbe41851
esaki01 <social0430@gmail.com>
2020-03-14 20:03:32 +0900

    Add test data

--- scripts/02_insert_test_data.sql
+++ scripts/02_insert_test_data.sql
@@ -1,4 +1,7 @@
 START TRANSACTION;
-INSERT INTO tests(name) VALUES('test data1');
-INSERT INTO tests(name) VALUES('test data2');
+INSERT INTO tests(name, date) VALUES('test data1', '2020-01-01');
+INSERT INTO tests(name, date) VALUES('test data2', '2020-02-02');
+INSERT INTO tests(name, date) VALUES('test data3', '2020-03-03');
+INSERT INTO tests(name, date) VALUES('test data4', '2020-04-04');
+INSERT INTO tests(name, date) VALUES('test data5', '2020-05-05');
 COMMIT;
