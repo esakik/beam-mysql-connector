1a643e3f4a5cfa6b4f548aa0ab05d327a1b45de3
esaki01 <social0430@gmail.com>
2020-03-14 14:38:30 +0900

    Change splitter name

--- examples/read_records_pipeline.py
+++ examples/read_records_pipeline.py
@@ -11,7 +11,7 @@ class ReadRecordsOptions(PipelineOptions):
     @classmethod
     def _add_argparse_args(cls, parser):
         parser.add_value_provider_argument("--host", dest="host", default="localhost")
-        parser.add_value_provider_argument("--port", dest="port", default=3306)
+        parser.add_value_provider_argument("--port", dest="port", default=3307)
         parser.add_value_provider_argument("--database", dest="database", default="test_db")
         parser.add_value_provider_argument("--query", dest="query", default="SELECT * FROM test_db.tests;")
         parser.add_value_provider_argument("--user", dest="user", default="root")
@@ -31,7 +31,7 @@ def run():
         user=options.user,
         password=options.password,
         port=options.port,
-        splitter=splitters.DefaultSplitter(),  # you can select how to split query from splitters
+        splitter=splitters.NoSplitter(),  # you can select how to split query from splitters
     )
 
     (
